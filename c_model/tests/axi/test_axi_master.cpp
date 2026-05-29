362de7565dfb8c9b1c876266d3e7079c8eb580e0
yue831107 <mwzg1107@gmail.com>
2026-05-29 18:50:47 +0800

    test(c_model): AxiMaster executes single write transaction with callback

--- c_model/tests/axi/test_axi_master.cpp
+++ c_model/tests/axi/test_axi_master.cpp
@@ -127,3 +127,41 @@ transactions:
       yaml, mock, std::string(::testing::TempDir()) + "/r_out.txt", 1, 1);
   EXPECT_FALSE(master.done());
 }
+
+TEST_F(AxiMasterTest, SingleWriteTransactionExecutes) {
+  auto wpath = std::string(::testing::TempDir()) + "/w_single.txt";
+  std::ofstream(wpath) << "01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 "
+                          "11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E 1F 20\n";
+  auto yaml = write_tmp(std::string(R"YAML(
+transactions:
+  - op: write
+    addr: 0x0
+    id: 0x7
+    len: 0
+    size: 5
+    burst: INCR
+    data_file: )YAML") + wpath + "\n");
+
+  ni::cmodel::axi::testing::MockSlave mock;
+  axi::AxiMasterT<ni::cmodel::axi::testing::MockSlave> master(
+      yaml, mock, std::string(::testing::TempDir()) + "/r.txt");
+
+  bool fired = false;
+  master.on_write_completed([&](const axi::WriteResult& r) {
+    fired = true;
+    EXPECT_EQ(r.id, 7);
+    EXPECT_EQ(r.resp, axi::Resp::OKAY);
+  });
+
+  master.tick();
+  EXPECT_EQ(mock.captured_aw.size(), 1u);
+  EXPECT_EQ(mock.captured_w.size(),  1u);
+  EXPECT_EQ(mock.captured_aw[0].id, 7);
+  EXPECT_EQ(mock.captured_w[0].data[0], 0x01);
+  EXPECT_EQ(mock.captured_w[0].last, true);
+
+  mock.queued_b.push_back(axi::BBeat{7, axi::Resp::OKAY, 0});
+  master.tick();
+  EXPECT_TRUE(fired);
+  EXPECT_TRUE(master.done());
+}
